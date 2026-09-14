9c7393e3ee724930c4ed0994e3dcdfcf0073ac70
Tarun Deshmukh <tarunbit2002@gmail.com>
2026-09-14 22:42:07 +0530

    ADD TO CART AND CART LIST TOATL CART ADDED

--- src/main/java/com/tarun/nest/controller/CartController.java
+++ src/main/java/com/tarun/nest/controller/CartController.java
@@ -2,17 +2,21 @@ package com.tarun.nest.controller;
 
 import com.tarun.nest.dto.AddToCartRequest;
 import com.tarun.nest.dto.AddToCartResponse;
+import com.tarun.nest.dto.CartResponse;
 import com.tarun.nest.entity.CartItem;
 import com.tarun.nest.service.CartService;
 import com.tarun.nest.util.AuthorizationUtil;
 
 import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -22,6 +26,7 @@ public class CartController {
     private final CartService cartService;
     private final AuthorizationUtil authorizationUtil;
 
+    // Add product to customer's cart
     @PostMapping("/items")
     public ResponseEntity<AddToCartResponse> addToCart(
             @Valid @RequestBody AddToCartRequest request,
@@ -49,5 +54,43 @@ public class CartController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    // Get all cart items + total cart items
+    @GetMapping
+    public ResponseEntity<CartResponse> getCart(
+            Authentication authentication
+    ) {
+
+        Long userId = authorizationUtil.getUserId(authentication);
+
+        if (userId == null) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .build();
+        }
+
+        List<CartItem> cartItems = cartService.getCart(userId);
+
+        List<AddToCartResponse> items = cartItems.stream()
+                .map(cartItem -> new AddToCartResponse(
+                        cartItem.getId(),
+                        cartItem.getProduct().getId(),
+                        cartItem.getProduct().getName(),
+                        cartItem.getProduct().getPrice(),
+                        cartItem.getQuantity()
+                ))
+                .toList();
+
+        int totalCartItems = cartItems.stream()
+                .mapToInt(CartItem::getQuantity)
+                .sum();
+
+        CartResponse response = new CartResponse(
+                items,
+                totalCartItems
+        );
+
+        return ResponseEntity.ok(response);
     }
 }
